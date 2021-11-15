26ab42f34a84541e6d8ffc0f740f48d5628f4d58
Shams-dyn Azarfane <shams-dyn.azarfane@etu.u-bordeaux.fr>
2021-11-15 16:37:47 +0100

    Update get_state.

--- game_test_sazarfane.c
+++ game_test_sazarfane.c
@@ -120,40 +120,27 @@ bool test_game_get_set_square(){
 /* ********** TEST GAME_GET_STATE ********** */
 
 bool test_game_get_state(){
-    //Création d'un array avec tout les squares
-    square array_with_all[DEFAULT_SIZE*DEFAULT_SIZE]= {
-        S_BLANK ,S_BLACK,S_BLACK0,S_BLACK1,S_BLACK2,S_BLACK3,S_BLACK4,
-        S_BLACKU,S_LIGHTBULB,S_MARK,S_BLANK | F_LIGHTED,S_LIGHTBULB | F_LIGHTED | F_ERROR ,S_MARK | F_LIGHTED ,S_BLANK,
-        S_BLACK0 | F_ERROR,S_BLACK1 | F_ERROR,S_BLACK2 | F_ERROR,S_BLACK3 | F_ERROR,S_BLACK4 | F_ERROR,S_BLACK | F_ERROR,S_BLACKU,
-        S_BLACKU,S_LIGHTBULB,S_MARK,S_BLANK,S_BLANK,S_BLANK,S_BLANK,
-        S_BLANK,S_BLACK,S_BLACK0,S_BLACK1,S_BLACK2,S_BLACK3,S_BLACK4,
-        S_BLACKU,S_LIGHTBULB,S_MARK,S_BLANK,S_BLANK,S_BLANK,S_BLANK,
-        S_BLANK,S_BLACK,S_BLACK0,S_BLACK1,S_BLACK2,S_BLACK3,S_BLACK4,
-        };
-    //Création du game
-    game game_test = game_new(array_with_all);
-    //Test si chaque square est correctement retourné
-    ASSERT(game_get_state(game_test,0,0) == S_BLANK);
-    ASSERT(game_get_state(game_test,0,1) == S_BLACK);
-    ASSERT(game_get_state(game_test,0,2) == S_BLACK0);
-    ASSERT(game_get_state(game_test,0,3) == S_BLACK1);
-    ASSERT(game_get_state(game_test,0,4) == S_BLACK2);
-    ASSERT(game_get_state(game_test,0,5) == S_BLACK3);
-    ASSERT(game_get_state(game_test,0,6) == S_BLACK4);
-    ASSERT(game_get_state(game_test,1,0) == S_BLACKU);
-    ASSERT(game_get_state(game_test,1,1) == S_LIGHTBULB);
-    ASSERT(game_get_state(game_test,1,2) == S_MARK);
-
-    ASSERT(game_get_state(game_test,1,3) == S_BLANK);
-    ASSERT(game_get_state(game_test,1,4) == S_LIGHTBULB);
-    ASSERT(game_get_state(game_test,1,5) == S_MARK);
-    
-    ASSERT(game_get_state(game_test,2,0) == S_BLACK0);
-    ASSERT(game_get_state(game_test,2,1) == S_BLACK1);
-    ASSERT(game_get_state(game_test,2,2) == S_BLACK2);
-    ASSERT(game_get_state(game_test,2,3) == S_BLACK3);
-    ASSERT(game_get_state(game_test,2,4) == S_BLACK4);
-    ASSERT(game_get_state(game_test,2,5) == S_BLACK);
+    //Création d'un jeu vide
+    game game_test = game_new_empty();
+    square array_all_square[20]={
+        S_BLANK ,S_BLACK,S_BLACK0,S_BLACK1,S_BLACK2,S_BLACK3,S_BLACK4,S_BLACKU,
+        S_LIGHTBULB,S_MARK,S_BLANK | F_LIGHTED,S_LIGHTBULB | F_LIGHTED | F_ERROR ,S_MARK | F_LIGHTED ,
+        S_BLACK0 | F_ERROR,S_BLACK1 | F_ERROR,S_BLACK2 | F_ERROR,S_BLACK3 | F_ERROR,S_BLACK4 | F_ERROR,S_BLACK | F_ERROR
+    };
+    square array_all_state[20]={
+        S_BLANK ,S_BLACK,S_BLACK0,S_BLACK1,S_BLACK2,S_BLACK3,S_BLACK4,S_BLACKU,
+        S_LIGHTBULB,S_MARK,S_BLANK,S_LIGHTBULB,S_MARK ,
+        S_BLACK0,S_BLACK1,S_BLACK2,S_BLACK3,S_BLACK4,S_BLACK
+    };
+    //Remplir de game_test avec les valeurs de array_all_values
+    for (int y = 0; y < DEFAULT_SIZE; y++){
+        for (int x = 0; x < DEFAULT_SIZE;x++){
+            for (int z = 0; z < 20;z++){
+            game_set_square(game_test,x,y,array_all_square[z]);
+            ASSERT(game_get_state(game_test,x,y) == array_all_state[z]); 
+            }
+        }
+    }
     return true;
 }
 
