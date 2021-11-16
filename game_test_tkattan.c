5c7a71ec5f12da5c14cc3d72bb1222c67f72a985
Taleiven Kattan <taleiven.kattan@etu.u-bordeaux.fr>
2021-11-16 15:53:54 +0100

    Added test of game_restart

--- game_test_tkattan.c
+++ game_test_tkattan.c
@@ -10,36 +10,83 @@
 
 /* ********** TEST GAME CHECK MOVE ********** */
 
-bool test_game_check_move(void)
+bool test_game_check_move()
 {
     return true;
 }
 
 /* ********** TEST GAME PLAY MOVE ********** */
 
-bool test_game_play_move(void)
+bool test_game_play_move()
 {
     return true;
 }
 
 /* ********** TEST GAME UPDATE FLAGS ********** */
 
-bool test_game_update_flags(void)
+bool test_game_update_flags()
 {
     return true;
 }
 
 /* ********** TEST GAME IS OVER ********** */
 
-bool test_game_is_over(void)
+bool test_game_is_over()
 {
     return true;
 }
 
 /* ********** TEST GAME RESTART ********** */
 
-bool test_game_restart(void)
+bool test_game_restart()
 {
+    // [1] Test game default and game default solution
+    game gameDefault = game_default();
+    game gameDefaultSolution = game_default_solution();
+    game_restart(gameDefaultSolution);
+
+    // restarting game default solution should give game default
+    ASSERT(game_equal(gameDefault, gameDefaultSolution));
+
+    // cean-up test game default and game default solution
+    game_delete(gameDefault);
+    game_delete(gameDefaultSolution);
+
+    // [2] Test of game restart on each square for each value
+    square *allSquares = create_array_all_values();
+    game testGame = game_new_empty();
+
+    // test each square
+    for (uint height = 0; height < DEFAULT_SIZE; height++)
+    {
+        for (uint width = 0; width < DEFAULT_SIZE; width++)
+        {
+            // for each possible value of a square
+            for (uint i = 0; i < SIZE_ALL_VALUES; i++)
+            {
+                // set the square to the value and restart it
+                game_set_square(testGame, height, width, allSquares[i]);
+                // use state to test as in all situations the flags need to be removed
+                square squareState = game_get_state(testGame, height, width);
+
+                game_restart(testGame);
+        
+                // if it is black then check that it stays black else it should be blank
+                if (squareState & S_BLACK)
+                    ASSERT(game_get_square(testGame, height, width) == squareState);
+                else
+                    ASSERT(game_get_square(testGame, height, width) == S_BLANK);
+            }
+
+            // reset square to empty
+            game_set_square(testGame, height, width, S_BLANK);
+        }
+    }
+
+    // Clean-up test game restart on each square for each value
+    free(allSquares);
+    game_delete(testGame);
+
     return true;
 }
 
