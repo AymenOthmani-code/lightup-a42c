bccdfa0821c8723c3dc04e6beefa04b637bf9bbd
Shams-dyn Azarfane <shams-dyn.azarfane@etu.u-bordeaux.fr>
2021-11-24 14:32:03 +0100

    Update parameters

--- game.c
+++ game.c
@@ -62,7 +62,7 @@ bool game_is_lightbulb(cgame g, uint i, uint j) { return false; }
 bool game_is_black(cgame g, uint i, uint j) { return false; }
 
 int game_get_black_number(cgame g, uint i, uint j) {
-    // Parameters Validations
+    // Validate parameters
     assert(g != NULL);
     assert(i < g->height && i >= 0); // check row parameter
     assert(j < g->width && j >= 0);  // check column parameter
